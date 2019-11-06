a32abfe3b606dff17e621bd2a9688b222e20a4ac
Mobeen Asghar <m.mobeen001@gmail.com>
2019-11-06 21:50:05 +0500

    Program for Multiplication Times Table

---
+++ pre-defined-function-with-(for-loop).cpp
@@ -0,0 +1,21 @@
+//In this program PRE-DEFINED function is used with for-loop
+
+#include <iostream>
+using namespace std;
+
+void table(int x)
+{
+	int y;
+	for (y = 1; y <= 10; y++)
+		cout << x << " * " << y << " = " << x * y << endl;
+}
+void main()
+{
+	int x;
+	cout << "(Simple Program for Multiplication Times Table with Range x10)" << endl;
+	cout << "Enter the No. for Table: ";
+	cin >> x;
+	table(x);
+system("pause");
+}
+ 
